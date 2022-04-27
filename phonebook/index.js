f3b7ee30f657f840b8d8b11ec23835582f851895
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-27 19:31:17 +0530

    Exercise 3.7

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 // Middleware
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
