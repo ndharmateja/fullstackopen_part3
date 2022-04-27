fa59cbc2e0319910e374ff150a5c4aa6dfcfcafe
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-27 14:45:51 +0530

    Exercise 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -31,6 +31,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const person = persons.find((person) => person.id === id)
+  return person ? response.json(person) : response.status(404).end()
+})
+
 app.get('/info', (request, response) => {
   response.send(
     `<div>Phonebook has info for ${
