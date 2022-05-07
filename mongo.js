9422b723780403e0db83b3dfe2e72ef8159a7b34
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-07 20:50:46 +0530

    Exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const uri = `mongodb+srv://fullstackopen:${password}@cluster0.d5aa1.mongodb.net/personsDb?retryWrites=true&w=majority`
+mongoose.connect(uri)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then((persons) => {
+    console.log('phonebook:')
+    persons.forEach(({ name, number }) => {
+      console.log(`${name} ${number}`)
+    })
+    mongoose.connection.close()
+  })
+}
