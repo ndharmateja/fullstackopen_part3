e7f8c9bfd47a11e3d20e7a08ed77a03843a17a6b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-08 19:49:30 +0530

    Exercises 3.15 and 3.16

--- index.js
+++ index.js
@@ -58,10 +58,10 @@ app.get('/info', (request, response) => {
   )
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -76,6 +76,17 @@ app.post('/api/persons', (request, response) => {
 
   const person = new Person({ name, number })
   person.save().then((savedPerson) => response.json(savedPerson))
+})
+
+app.use((error, request, response, next) => {
+  console.log(error.message)
+
+  // If cast error, we return bad request error
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'Malformed id' })
+
+  // Otherwise forward it to next mw
+  next(error)
 })
 
 const PORT = process.env.PORT || 3001
