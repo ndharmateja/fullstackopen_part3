a7de9116cdefbcdfe2b9cc82dece3ee54eec9e56
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-08 23:09:08 +0530

    Add es lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,10 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
   },
 }
